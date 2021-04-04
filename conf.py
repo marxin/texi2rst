9d8f26ab19e32fce4e376e0595bfae3b1452a16d
Martin Liska <mliska@suse.cz>
2021-04-04 09:22:18 +0200

    Display navigation buttons: both.

--- conf.py
+++ conf.py
@@ -96,7 +96,9 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'prev_next_buttons_location': 'both'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
